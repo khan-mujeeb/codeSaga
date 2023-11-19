4e73e65bf9d733f01e58ee2947ef7bd12a77848b
Rishi47Sharma <rishigolu321@gmail.com>
2023-11-19 21:55:20 +0530

    sql routes commented

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import UnitGame from "./pages/UnitGame";
 import FlexGame from "./pages/FlexGame";
 import SelectQuery from "./pages/sql";
 import {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-    Navigate,
-    Outlet,
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  Outlet,
 } from "react-router-dom";
 
 // import "./App.css";
@@ -22,26 +22,26 @@ import {
  *
  */
 const App = () => {
-    return (
-        <>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Rootlayout />}>
-                        <Route path="" element={<Navigate to={"CssMenu"} />} />
-                        <Route path="/CssMenu" element={<Outlet />}>
-                            <Route path="" element={<CssMenu />} />
-                            <Route path="unit" element={<UnitGame />} />
-                            <Route path="flex" element={<FlexGame />} />
-                            <Route path="sql" element={<SelectQuery />} />
-                        </Route>
-                    </Route>
-                </Routes>
-            </Router>
-            {/* <button type="button" className="login-with-google-btn">
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Rootlayout />}>
+            <Route path="" element={<Navigate to={"CssMenu"} />} />
+            <Route path="/CssMenu" element={<Outlet />}>
+              <Route path="" element={<CssMenu />} />
+              <Route path="unit" element={<UnitGame />} />
+              <Route path="flex" element={<FlexGame />} />
+              {/* <Route path="sql" element={<SelectQuery />} /> */}
+            </Route>
+          </Route>
+        </Routes>
+      </Router>
+      {/* <button type="button" className="login-with-google-btn">
         Sign in with Google
       </button> */}
-        </>
-    );
+    </>
+  );
 };
 
 export default App;
--- src/components/sqlGames/Table.jsx
+++ src/components/sqlGames/Table.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 const Table = ({result, getColumnNames}) => {
     return (
         <div>

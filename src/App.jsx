eabe7eb8613d357f5a4b1a82e544473c8a2c7a0a
khan-mujeeb <mujeebkhan1831@gmail.com>
2024-02-18 16:09:12 +0530

    preloader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 //import HomeScreen from "./components/HomeScreen"
 
-import AiInterview from "./pages/AiInterview.jsx"
+import AiInterview from "./pages/AiInterview.jsx";
+import AiInterview2 from "./pages/AiInterview2.jsx";
 import Rootlayout from "./layouts/rootlayout";
 import CssMenu from "./pages/CssMenu";
 import UnitGame from "./pages/UnitGame";
 import FlexGame from "./pages/FlexGame";
+import { Lines } from "react-preloaders";
 import Sql from "./pages/Sql.jsx";
 import {
     BrowserRouter as Router,
@@ -13,35 +15,40 @@ import {
     Navigate,
     Outlet,
 } from "react-router-dom";
+import React from "react";
 
-// import "./App.css";
 
-/**
- * - The App component is the entry point of the application.
- * - It is responsible for rendering the top-level components.
- * - It is also responsible for routing.
- *
- */
 const App = () => {
     return (
         <>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Rootlayout />}>
-                        <Route path="" element={<Navigate to={"CssMenu"} />} />
-                        <Route path="/CssMenu" element={<Outlet />}>
-                            <Route path="" element={<CssMenu />} />
-                            <Route path="unit" element={<UnitGame />} />
-                            <Route path="flex" element={<FlexGame />} />
-                            <Route path="sql" element={<Sql />} />
-                            <Route path="ai-interview" element={<AiInterview />} />
+            <React.Fragment>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<Rootlayout />}>
+                            <Route
+                                path=""
+                                element={<Navigate to={"CssMenu"} />}
+                            />
+                            <Route path="/CssMenu" element={<Outlet />}>
+                                <Route path="" element={<CssMenu />} />
+                                <Route path="unit" element={<UnitGame />} />
+                                <Route path="flex" element={<FlexGame />} />
+                                <Route path="sql" element={<Sql />} />
+                                <Route
+                                    path="ai-interview"
+                                    element={<AiInterview />}
+                                />
+                                <Route
+                                    path="ai-interview2"
+                                    element={<AiInterview2 />}
+                                />
+                            </Route>
                         </Route>
-                    </Route>
-                </Routes>
-            </Router>
-            {/* <button type="button" className="login-with-google-btn">
-        Sign in with Google
-      </button> */}
+                    </Routes>
+                </Router>
+                <Lines />
+            </React.Fragment>
+            
         </>
     );
 };

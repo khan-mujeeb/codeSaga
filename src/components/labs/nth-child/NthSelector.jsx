dd8967c60d834990f23f390311355bc23f5367d8
khan-mujeeb <mujeebkhan1831@gmail.com>
2024-05-27 21:41:11 +0530

    nth selector design change

--- src/components/labs/nth-child/NthSelector.jsx
+++ src/components/labs/nth-child/NthSelector.jsx
@@ -1,98 +1,106 @@
-import React from "react";
+import Star from "../../Back/Star";
 import "./NthSelector.css";
 
 function App() {
     return (
-        <div className="flex flex-col items-center">
-             <h1 className="text-4xl font-semibold">CSS Transition Timing Function</h1>
-            <p className="text-xl text-gray-700">Hover to see the transition</p>
-            <div className="nth-child-container">
-                <div className="nth-child-left">
-                    <div className="nth-child-wrapper">
-                        <h3>:first-child</h3>
-                        <div className="nth-child-box-container first-child">
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                        </div>
-                    </div>
+        <>
+        < Star />
+            <div className="flex flex-col items-center justify-center bg-black w-screen h-screen">
+                <h1 className="text-4xl font-semibold">
+                    Nth Child Selector in CSS
+                </h1>
+                <p className="text-xl text-gray-300">
+                    Hover to see the transition
+                </p>
 
-                    <div className="nth-child-wrapper">
-                        <h3>:last-child</h3>
-                        <div className="nth-child-box-container last-child">
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
+                <div className=" bg-whiteflex flex-col items-center justify-center nth-child-container mt-10 w-full ">
+                    <div className=" nth-child-left ">
+                        <div className="nth-child-wrapper  w-[650px] flex justify-between">
+                           <h3 className="text-xl text-white">:first-child</h3>
+                            <div className="nth-child-box-container first-child ">
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="nth-child-wrapper">
-                        <h3>:nth-child(2)</h3>
-                        <div className="nth-child-box-container nth-child-second">
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
+                        <div className="nth-child-wrapper w-[650px] flex justify-between">
+                           <h3 className="text-xl text-white">:last-child</h3>
+                            <div className="nth-child-box-container last-child">
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="nth-child-wrapper">
-                        <h3>:nth-child(odd)</h3>
-                        <div className="nth-child-box-container nth-child-odd">
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
+                        <div className="nth-child-wrapper w-[650px] flex justify-between">
+                           <h3 className="text-xl text-white">:nth-child(2)</h3>
+                            <div className="nth-child-box-container nth-child-second">
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                            </div>
                         </div>
-                    </div>
-                    <div className="nth-child-wrapper">
-                        <h3>:nth-child(even)</h3>
-                        <div className="nth-child-box-container nth-child-even">
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                        </div>
-                    </div>
 
-                    <div className="nth-child-wrapper">
-                        <h3>:nth-child(3n)</h3>
-                        <div className="nth-child-box-container nth-child-3n">
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
+                        <div className="nth-child-wrapper w-[650px] flex justify-between">
+                           <h3 className="text-xl text-white">:nth-child(odd)</h3>
+                            <div className="nth-child-box-container nth-child-odd">
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                            </div>
                         </div>
-                    </div>
+                        <div className="nth-child-wrapper w-[650px] flex justify-between">
+                           <h3 className="text-xl text-white">:nth-child(even)</h3>
+                            <div className="nth-child-box-container nth-child-even">
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                            </div>
+                        </div>
 
-                    <div className="nth-child-wrapper">
-                        <h3>:nth-child(n+3)</h3>
-                        <div className="nth-child-box-container nth-child-other">
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
-                            <div className="nth-child-box"></div>
+                        <div className="nth-child-wrapper w-[650px] flex justify-between">
+                           <h3 className="text-xl text-white">:nth-child(3n)</h3>
+                            <div className="nth-child-box-container nth-child-3n">
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                            </div>
+                        </div>
+
+                        <div className="nth-child-wrapper w-[650px] flex justify-between">
+                           <h3 className="text-xl text-white">:nth-child(n+3)</h3>
+                            <div className="nth-child-box-container nth-child-other">
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                                <div className="nth-child-box"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 

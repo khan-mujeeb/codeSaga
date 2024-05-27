2d2faf720ee0bd910b052cc7a0a055b3a0443ecd
khan-mujeeb <mujeebkhan1831@gmail.com>
2024-05-27 21:45:53 +0530

    heading added

--- src/components/labs/nth-child/NthSelector.jsx
+++ src/components/labs/nth-child/NthSelector.jsx
@@ -4,19 +4,16 @@ import "./NthSelector.css";
 function App() {
     return (
         <>
-        < Star />
+            <Star />
             <div className="flex flex-col items-center justify-center bg-black w-screen h-screen">
-                <h1 className="text-4xl font-semibold">
+                <div className=" w-screen text-[#EB6F63]  font-semibold   items-center flex justify-center  text-center text-7xl drop-shadow-[4px_4px_var(--tw-shadow-color)] shadow-[#8ECEBA]">
                     Nth Child Selector in CSS
-                </h1>
-                <p className="text-xl text-gray-300">
-                    Hover to see the transition
-                </p>
+                </div>
 
                 <div className=" bg-whiteflex flex-col items-center justify-center nth-child-container mt-10 w-full ">
                     <div className=" nth-child-left ">
                         <div className="nth-child-wrapper  w-[650px] flex justify-between">
-                           <h3 className="text-xl text-white">:first-child</h3>
+                            <h3 className="text-xl text-white">:first-child</h3>
                             <div className="nth-child-box-container first-child ">
                                 <div className="nth-child-box"></div>
                                 <div className="nth-child-box"></div>
@@ -28,7 +25,7 @@ function App() {
                         </div>
 
                         <div className="nth-child-wrapper w-[650px] flex justify-between">
-                           <h3 className="text-xl text-white">:last-child</h3>
+                            <h3 className="text-xl text-white">:last-child</h3>
                             <div className="nth-child-box-container last-child">
                                 <div className="nth-child-box"></div>
                                 <div className="nth-child-box"></div>
@@ -40,7 +37,9 @@ function App() {
                         </div>
 
                         <div className="nth-child-wrapper w-[650px] flex justify-between">
-                           <h3 className="text-xl text-white">:nth-child(2)</h3>
+                            <h3 className="text-xl text-white">
+                                :nth-child(2)
+                            </h3>
                             <div className="nth-child-box-container nth-child-second">
                                 <div className="nth-child-box"></div>
                                 <div className="nth-child-box"></div>
@@ -52,7 +51,9 @@ function App() {
                         </div>
 
                         <div className="nth-child-wrapper w-[650px] flex justify-between">
-                           <h3 className="text-xl text-white">:nth-child(odd)</h3>
+                            <h3 className="text-xl text-white">
+                                :nth-child(odd)
+                            </h3>
                             <div className="nth-child-box-container nth-child-odd">
                                 <div className="nth-child-box"></div>
                                 <div className="nth-child-box"></div>
@@ -63,7 +64,9 @@ function App() {
                             </div>
                         </div>
                         <div className="nth-child-wrapper w-[650px] flex justify-between">
-                           <h3 className="text-xl text-white">:nth-child(even)</h3>
+                            <h3 className="text-xl text-white">
+                                :nth-child(even)
+                            </h3>
                             <div className="nth-child-box-container nth-child-even">
                                 <div className="nth-child-box"></div>
                                 <div className="nth-child-box"></div>
@@ -75,7 +78,9 @@ function App() {
                         </div>
 
                         <div className="nth-child-wrapper w-[650px] flex justify-between">
-                           <h3 className="text-xl text-white">:nth-child(3n)</h3>
+                            <h3 className="text-xl text-white">
+                                :nth-child(3n)
+                            </h3>
                             <div className="nth-child-box-container nth-child-3n">
                                 <div className="nth-child-box"></div>
                                 <div className="nth-child-box"></div>
@@ -87,7 +92,9 @@ function App() {
                         </div>
 
                         <div className="nth-child-wrapper w-[650px] flex justify-between">
-                           <h3 className="text-xl text-white">:nth-child(n+3)</h3>
+                            <h3 className="text-xl text-white">
+                                :nth-child(n+3)
+                            </h3>
                             <div className="nth-child-box-container nth-child-other">
                                 <div className="nth-child-box"></div>
                                 <div className="nth-child-box"></div>

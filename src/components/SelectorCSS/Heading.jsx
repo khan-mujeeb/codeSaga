b7a0a6ba0605bcaeef852c3dbd3a7c23fc783db5
Pravin Kale <pravinkale264@gmail.com>
2024-05-27 19:32:18 +0530

    selector modify

--- src/components/SelectorCSS/Heading.jsx
+++ src/components/SelectorCSS/Heading.jsx
@@ -4,9 +4,9 @@ function Heading({ data }) {
   return (
     <div
       id="heading"
-      className="w-full flex bg-[#043C52] justify-between h-[10%] px-6 py-4 "
+      className="w-full flex bg-[#043C52] justify-center items-center gap-24  h-[10%] px-6 py-4 "
     >
-      <h1 className=" text-2xl   text-white font-bold">CSS SCOOPS</h1>
+      <h1 className=" text-2xl text-white font-bold">CSS SCOOPS</h1>
       <h5 className=" text-white">Level {data} of 5</h5>
     </div>
   );
